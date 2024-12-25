68a932e6eff538919fd0ab539aa7264dd04c8404
chisssel <platov.artem.15@gmail.com>
2024-12-26 00:44:22 +0300

    lessons 1-4 complite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="../assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>`,
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent],
 })
 export class AppComponent {
-  title = 'angularAppStages';
+  title = 'homes';
 }
